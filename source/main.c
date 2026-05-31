c9b4aeb192215c477d8007d776ba1933a8506dac
Hayumin <hayumigumi@gmail.com>
2026-05-31 05:20:27 -0600

    Base: Non-Colliding Player

--- source/main.c
+++ source/main.c
@@ -8,9 +8,29 @@
 
 #include <tonc.h>
 #include "BG.h"
+#include "human.h"
+
+#include "player.h"
 
 #define MAX_X_SCROLL 16
 #define MAX_Y_SCROLL 96
+
+
+typedef struct VIEWPORT
+{
+	int x, xmin, xmax, xpage;
+	int y, ymin, ymax, ypage;
+} VIEWPORT;
+
+VIEWPORT g_vp=
+{
+	0, 0, 1024, 240,
+	0, 0, 1024, 160,
+};
+
+
+OBJ_ATTR obj_buffer[128];
+TSprite g_link;
 
 
 void MetaTileLoad(u16 MetaX, u16 MetaY, u16 TileID);
@@ -20,11 +40,17 @@ int main()
 	// Init interrupts and VBlank irq.
 	irq_init(NULL);
 	irq_add(II_VBLANK, NULL);
+	oam_init(obj_buffer, 128);
 
 	// Load palette
 	memcpy16(pal_bg_mem, BGPal, BGPalLen / sizeof(u16));
 	// Load tiles into CBB 0
 	memcpy32(&tile_mem[0][0], BGTiles, BGTilesLen / sizeof(u32));
+	
+	GRIT_CPY(pal_obj_mem, humanPal);
+	GRIT_CPY(tile_mem[4], humanTiles);
+
+	player_init(&g_link, int2fx(120), int2fx(80), 0);
 	
 	// Load map into SBB 30
 	for (u16 i = 0; i < 16; i++)
@@ -41,19 +67,38 @@ int main()
 	// set up BG0 for a 4bpp 32x32t(256bx256b) map, using
 	//   using charblock 0 and screenblock 31
 	REG_BG0CNT= BG_CBB(0) | BG_SBB(30) | BG_4BPP | BG_REG_32x32;
-	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ |
+		DCNT_OBJ_1D;
 
 	// Scroll around some
 	int x= 0, y=0;
 	bool HammerMode=true;
+	bool ScrollMode=false;
 	while(1)
 	{
 		VBlankIntrWait();
 		key_poll();
-
-		x += key_tri_horz();
-		y += key_tri_vert();
-		if(key_hit(KEY_A)){
+		
+		
+		if(key_hit(KEY_START)){
+			ScrollMode=!ScrollMode;	
+		}
+		if(ScrollMode){
+				x += key_tri_horz();
+				y += key_tri_vert();
+				if(x<0){x=0;}
+				if(x>MAX_X_SCROLL){x=MAX_X_SCROLL;}
+				if(y<0){y=0;}
+				if(y>MAX_Y_SCROLL){y=MAX_Y_SCROLL;}
+				REG_BG0HOFS= x;
+				REG_BG0VOFS= y;
+		}else{
+			player_input(&g_link);
+			player_turn(&g_link);
+			player_move(&g_link);
+		}
+		oam_copy(oam_mem, obj_buffer, 128);
+		if(key_hit(KEY_SELECT)){
 			HammerMode=!HammerMode;
 			if(HammerMode){
 				MetaTileLoad(6,5,0x01);
@@ -63,12 +108,6 @@ int main()
 				MetaTileLoad(8,5,0x02);
 			}
 		}
-		if(x<0){x=0;}
-		if(x>MAX_X_SCROLL){x=MAX_X_SCROLL;}
-		if(y<0){y=0;}
-		if(y>MAX_Y_SCROLL){y=MAX_Y_SCROLL;}
-		REG_BG0HOFS= x;
-		REG_BG0VOFS= y;
 	}
 
 	return 0;
---
+++ source/player.c
@@ -0,0 +1,101 @@
+#include <tonc.h>
+
+#include "player.h"
+
+#define LINK_SPEED	0x0f00
+
+// --------------------------------------------------------------------
+// EXTERNAL
+// --------------------------------------------------------------------
+
+typedef struct VIEWPORT
+{
+	int x, xmin, xmax, xpage;
+	int y, ymin, ymax, ypage;
+} VIEWPORT;
+
+extern VIEWPORT g_vp;
+extern OBJ_ATTR obj_buffer[];
+
+// --------------------------------------------------------------------
+// DECLARATIONS
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// LUTS
+// --------------------------------------------------------------------
+
+const u8 cLookDirs[4]= { 0, 1, 3, 2 };
+// Default link attributes
+const OBJ_ATTR cLinkObjs=
+{	
+	0, 
+	ATTR1_SIZE_16,
+	4,
+	0 //Don't Touch  Field 3		
+};
+
+
+// --------------------------------------------------------------------
+// FUNCTIONS
+// --------------------------------------------------------------------
+
+void player_init(TSprite *link, u32 x, u32 y, int obj_id)
+{
+	link->x= x;
+	link->y= y;
+	link->vx= link->vy= 0;
+
+	link->dir= LOOK_DOWN;
+	link->objId= obj_id;
+
+	oam_copy(&obj_buffer[obj_id], &cLinkObjs, 1);
+}
+
+void player_input(TSprite *link)
+{
+	link->vx= link->vy= 0;
+
+	if( key_hit(KEY_RIGHT) )
+	{
+		link->vx= LINK_SPEED;
+		link->dir= LOOK_RIGHT;
+	}
+	else if( key_hit(KEY_LEFT) )
+	{
+		link->vx= -LINK_SPEED;
+		link->dir= LOOK_LEFT;
+	}
+
+	if( key_hit(KEY_DOWN) )
+	{
+		link->vy= LINK_SPEED;
+		link->dir= LOOK_DOWN;
+	}
+	else if( key_hit(KEY_UP) )
+	{
+		link->vy= -LINK_SPEED;
+		link->dir= LOOK_UP;
+	}
+}
+
+void player_move(TSprite *link)
+{
+	// TODO : collision testing here?
+
+	link->x += link->vx;
+	link->y += link->vy;
+}
+
+void player_turn(TSprite *link){
+	POINT pt= { fx2int(link->x) - g_vp.x, fx2int(link->y) - g_vp.y };
+	OBJ_ATTR *obj= &obj_buffer[link->objId];
+	int dir= link->dir;
+
+	BFN_SET(obj[0].attr0, pt.y, ATTR0_Y);
+
+	obj[0].attr1 &= ~(ATTR1_HFLIP | ATTR1_X_MASK);
+	obj[0].attr1 |= BFN_PREP(pt.x, ATTR1_X);
+	obj[0].attr2=dir*4;
+}
+
+// EOF
