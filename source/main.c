e0825a87b02deaa6fffde86478a9ec842e27dadc
Hayumin <hayumigumi@gmail.com>
2026-06-03 00:08:34 -0600

    Strip Offset to Another function

--- source/main.c
+++ source/main.c
@@ -103,7 +103,7 @@ void wallt_meta_init(TMapInfo *bgt, int bgnr, u32 ctrl,
 	for (u32 i = 0; i < map_height; i++){
 		for (u32 x = 0; x < map_width; x++)
 		{
-			MetaTileLoad(x,i,0,dst,inanimatesMetaTiles, 0);
+			MetaTileLoad(x,i,0,dst,inanimatesMetaTiles);
 		}
 	}
 }
@@ -130,7 +130,7 @@ void bgt_meta_init(TMapInfo *bgt, int bgnr, u32 ctrl,
 	for (u32 i = 0; i < map_height; i++){
 		for (u32 x = 0; x < map_width; x++)
 		{
-			MetaTileLoad(x,i,src[i*tileSize+x],dst, inanimatesMetaTiles, 0);
+			MetaTileLoad(x,i,src[i*tileSize+x],dst, inanimatesMetaTiles);
 		}
 	}
 }
@@ -203,55 +203,55 @@ int main()
 		if(key_hit(KEY_START)){
 			WallMode=!WallMode;
 			if(WallMode){
-				MetaTileLoad(3,3,0x08, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(3,7,0x07, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(8,11,0x06, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(11,5,0x0A, g_walls.dstMap, inanimatesMetaTiles, 0);
+				MetaTileLoad(3,3,0x08, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(3,7,0x07, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(8,11,0x06, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(11,5,0x0A, g_walls.dstMap, inanimatesMetaTiles);
 				if(HammerMode){
-					MetaTileLoad(7,5,0x04, g_walls.dstMap, inanimatesMetaTiles, 0);
+					MetaTileLoad(7,5,0x04, g_walls.dstMap, inanimatesMetaTiles);
 				}else{
-					MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
+					MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles);
 				}
 			}else{
-				MetaTileLoad(3,3,0x09, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(3,7,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(8,11,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(11,5,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
+				MetaTileLoad(3,3,0x09, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(3,7,0x00, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(8,11,0x00, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(11,5,0x00, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles);
 			}
 		}
 		if(key_hit(KEY_SELECT)){
 			HammerMode=!HammerMode;
 			if(!HammerMode){
-				MetaTileLoad(6,5,0x01, g_bg.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(8,5,0x01, g_bg.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
+				MetaTileLoad(6,5,0x01, g_bg.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(8,5,0x01, g_bg.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles);
 			}else{
-				MetaTileLoad(6,5,0x02, g_bg.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(8,5,0x02, g_bg.dstMap, inanimatesMetaTiles, 0);
+				MetaTileLoad(6,5,0x02, g_bg.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(8,5,0x02, g_bg.dstMap, inanimatesMetaTiles);
 				if(WallMode){
-					MetaTileLoad(7,5,0x04, g_walls.dstMap, inanimatesMetaTiles, 0);
+					MetaTileLoad(7,5,0x04, g_walls.dstMap, inanimatesMetaTiles);
 				}else{
-					MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles, 0);
+					MetaTileLoad(7,5,0x00, g_walls.dstMap, inanimatesMetaTiles);
 				}
 			}
 		}
 		if(key_hit(KEY_A)){
 			ChestClosed=!ChestClosed;
 			if(ChestClosed){
-				MetaTileLoad(3,5,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(3,9,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(3,11,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(7,3,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(10,11,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
-				MetaTileLoad(11,11,0x05, g_walls.dstMap, inanimatesMetaTiles, 0);
+				MetaTileLoad(3,5,0x05, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(3,9,0x05, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(3,11,0x05, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(7,3,0x05, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(10,11,0x05, g_walls.dstMap, inanimatesMetaTiles);
+				MetaTileLoad(11,11,0x05, g_walls.dstMap, inanimatesMetaTiles);
 			}else{
-				MetaTileLoad(3,5,0x01, g_walls.dstMap, itemsMetaTiles, 0);
-				MetaTileLoad(3,9,0x02, g_walls.dstMap, itemsMetaTiles, 0);
-				MetaTileLoad(3,11,0x03, g_walls.dstMap, itemsMetaTiles, 0);
-				MetaTileLoad(7,3,0x04, g_walls.dstMap, itemsMetaTiles, 0);
-				MetaTileLoad(10,11,0x05, g_walls.dstMap, itemsMetaTiles, 0);
-				MetaTileLoad(11,11,0x06, g_walls.dstMap, itemsMetaTiles, 0);
+				MetaTileLoad(3,5,0x01, g_walls.dstMap, itemsMetaTiles);
+				MetaTileLoad(3,9,0x02, g_walls.dstMap, itemsMetaTiles);
+				MetaTileLoad(3,11,0x03, g_walls.dstMap, itemsMetaTiles);
+				MetaTileLoad(7,3,0x04, g_walls.dstMap, itemsMetaTiles);
+				MetaTileLoad(10,11,0x05, g_walls.dstMap, itemsMetaTiles);
+				MetaTileLoad(11,11,0x06, g_walls.dstMap, itemsMetaTiles);
 			}
 		}
 		
--- source/metatile.c
+++ source/metatile.c
@@ -1,7 +1,7 @@
 #include <tonc.h>
 #include "inanimates.h"
 
-bool MetaTileEquals(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en,  const unsigned short *MetaTiles, u16 offset){
+bool OffsetMetaTileEquals(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en,  const unsigned short *MetaTiles, u16 offset){
 	MetaX*=2;
 	MetaY*=2;
 	TileID*=4;
@@ -11,7 +11,7 @@ bool MetaTileEquals(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en,  const u
 	(se_en[(MetaY+1)*32+MetaX]==(MetaTiles[TileID+2]+offset))&&
 	(se_en[(MetaY+1)*32+MetaX+1]==(MetaTiles[TileID+3]+offset));
 }
-void MetaTileLoad(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en, const unsigned short *MetaTiles, u16 offset){
+void OffsetMetaTileLoad(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en, const unsigned short *MetaTiles, u16 offset){
 	MetaX*=2;
 	MetaY*=2;
 	TileID*=4;
@@ -19,4 +19,12 @@ void MetaTileLoad(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en, const unsi
 	se_en[MetaY*32+MetaX+1]=MetaTiles[TileID+1]+offset;
 	se_en[(MetaY+1)*32+MetaX]=MetaTiles[TileID+2]+offset;
 	se_en[(MetaY+1)*32+MetaX+1]=MetaTiles[TileID+3]+offset;
+}
+
+
+bool MetaTileEquals(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en,  const unsigned short *MetaTiles){
+	return OffsetMetaTileEquals(MetaX, MetaY, TileID, se_en, MetaTiles, 0);
+}
+void MetaTileLoad(u16 MetaX, u16 MetaY, u16 TileID, SCR_ENTRY *se_en, const unsigned short *MetaTiles){
+	return OffsetMetaTileLoad(MetaX, MetaY, TileID, se_en, MetaTiles, 0);
 }
