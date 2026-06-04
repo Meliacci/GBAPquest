ff8a0406cce3133cd301ad4a4f8eb76964956041
Hayumin <hayumigumi@gmail.com>
2026-06-04 13:27:01 -0600

    Adds Interaction Code

--- source/interactions.c
+++ source/interactions.c
@@ -2,6 +2,8 @@
 #include "interactions.h"
 #include "metatile.h"
 #include "hearts.h"
+#include "items.h"
+#include "inanimates.h"
 
 void InteractWith(TInteract* Interactable){
     if (Interactable)
@@ -14,16 +16,50 @@ void InteractWith(TInteract* Interactable){
             Interactable->type=EIT_NONE;
             break;
         case EIT_CHEST:
-            /* code */
+            if(Interactable->target>>8){
+                Interactable->MetaTiles=heartsMetaTiles;
+            }else{
+                Interactable->MetaTiles=itemsMetaTiles;
+            }
+            MetaTileLoad(Interactable->x,Interactable->y,Interactable->target&0xFF,Interactable->dst,Interactable->MetaTiles);
+            Interactable->type=EIT_ITEM;
+            Interactable->state=Interactable->target;
             break;
         case EIT_ENEMY:
-            /* code */
+            if(true){//Check that player has the Sword
+                Interactable->state=(Interactable->state)-1;
+                if(Interactable->state>0){
+                    MetaTileLoad(Interactable->x,Interactable->y,Interactable->state,Interactable->dst,Interactable->MetaTiles);
+                }else{
+                    if(Interactable->target>>8){
+                        Interactable->MetaTiles=heartsMetaTiles;
+                    }else{
+                        Interactable->MetaTiles=itemsMetaTiles;
+                    }
+                    if(Interactable->target&0xFF){
+                        MetaTileLoad(Interactable->x,Interactable->y,Interactable->target&0xFF,Interactable->dst,Interactable->MetaTiles);
+                        Interactable->type=EIT_ITEM;
+                        Interactable->state=Interactable->target;
+                    }else{
+                        Interactable->MetaTiles=inanimatesMetaTiles;
+                        MetaTileLoad(Interactable->x,Interactable->y,0x00,Interactable->dst,Interactable->MetaTiles);
+                        Interactable->type=EIT_NONE;
+                    }
+                }
+            }
             break;
         case EIT_ITEM:
-            /* code */
+            Interactable->MetaTiles=inanimatesMetaTiles;
+            MetaTileLoad(Interactable->x,Interactable->y,0x00,Interactable->dst,Interactable->MetaTiles);
+            Interactable->type=EIT_NONE;
+            Interactable->target; //Call to add Target Item into inventory
             break;
         case EIT_WALL:
-            /* code */
+            if (true) //Check item here
+            {   
+                MetaTileLoad(Interactable->x,Interactable->y,0x00,Interactable->dst,Interactable->MetaTiles);
+                Interactable->type=EIT_NONE;
+            }
             break;
 
         case EIT_NONE:
